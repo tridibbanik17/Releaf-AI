1c21cf6296057cb990258509043b5d609b189ae8
Eric Zhang <163900411+eric-z2@users.noreply.github.com>
2025-01-12 02:28:54 -0500

    fixed sign up

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -3,17 +3,17 @@ import './SignUp.css'
 function SignUp() {
     return (
         <div class="sign-up-container">
-    <h1>Sign Up</h1>
-    <form>
-      <div class="form-group">
-        <input type="text" id="username" name="username" placeholder="Enter your username" required/>
-      </div>
-      <div>
-        <input type="email" id="email" name="email" placeholder="Enter your email" required/>
-      </div>
-      <button type="submit" class="submit-btn">Sign Up</button>
-    </form>
-  </div>
+          <h1 class='center'>Sign Up</h1>
+          <form>
+            <div class="form-group">
+              <input type="text" id="username" name="username" placeholder="Enter your username" required />
+            </div>
+            <div>
+              <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+            </div>
+            <button type="submit" class="submit-btn">Sign Up</button>
+          </form>
+        </div>
     )
 }
 
